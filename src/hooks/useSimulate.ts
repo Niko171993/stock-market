8d83be46d3debc17738ccf195af4cfb33c3f28c6
unknown <nmacharashvili171993@gmail.com>
2024-01-02 07:15:48 -0800

    day 2 final

--- src/hooks/useSimulate.ts
+++ src/hooks/useSimulate.ts
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-const useSimulate = (currentAm: number) => {
-  const [newPrice, setNewPrice] = useState<number>(0);
-  const [currentAmount, setCurrentAmount] = useState<number>(currentAm);
+// const useSimulate = (currentAm: number) => {
+//   const [newPrice, setNewPrice] = useState<number>(0);
+//   const [currentAmount, setCurrentAmount] = useState<number>(currentAm);
 
-  const randomFactor = Math.random() * 10 - 5;
-  const newAmount = currentAmount * (1 + randomFactor / 100);
-  setNewPrice(() => newAmount);
+//   const randomFactor = Math.random() * 10 - 5;
+//   const newAmount = currentAmount * (1 + randomFactor / 100);
+//   setNewPrice(() => newAmount);
 
-  return newPrice;
-};
+//   return newPrice;
+// };
 
-export default useSimulate;
+// export default useSimulate;
